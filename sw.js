22f0495fe88eb62cbf5f28d5a619e4c8c554206d
stonepro-lang <lukestonepro@gmail.com>
2025-08-14 19:13:23 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'stockalerts-serverless-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const u = new URL(e.request.url);
+  if (u.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
